7e9c44bc566c809fd6dbf97185e75262a6b89f17
aevilesaguiar <aeviles@hotmail.com>
2021-12-02 20:18:47 -0300

    desafio

---
+++ java-fundamentos/src/classe/desafio/Comida.java
@@ -0,0 +1,17 @@
+package classe.desafio;
+
+public class Comida {
+
+		String nome;
+		double peso;
+		
+		//Construtor
+		Comida  (String varnome, double varpeso){
+			this.nome=varnome;
+			this.peso=varpeso;
+					
+
+
+	}
+
+}
