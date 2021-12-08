90b045132a9a8ec3675d8f24f06083ead8bd5219
aevilesaguiar <aeviles@hotmail.com>
2021-12-07 21:22:28 -0300

    Equals e hashcode

---
+++ java-fundamentos/src/Teste.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		  Scanner scan = new Scanner(System.in);
+	        int a = scan.nextInt();
+	        scan.close();
+	        
+	        
+	        
+
+	}
+
+}
---
+++ java-fundamentos/src/classe/EqualsEHasshCode.java
@@ -0,0 +1,22 @@
+package classe;
+
+public class EqualsEHasshCode {
+	
+	public static void main(String[] args) {
+		
+		Usuario u1= new Usuario();
+		u1.nome="Pedro Silva";
+		u1.email="pedro@gmail.com";
+		
+		Usuario u2= new Usuario();
+		u2.nome="Pedro Silva";
+		u2.email="pedro@gmail.com";
+		
+		System.out.println(u2==u1);
+		System.out.println(u1.equals(u2));
+		System.out.println(u2.equals(u1));
+		
+		
+	}
+
+}
